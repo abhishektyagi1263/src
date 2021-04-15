631046233a10f68d6764cb5114fd9a658614690a
abhishektyagi1263 <abhishektyagi1263@gmail.com>
2021-04-15 19:42:08 +0530

    accounts v1.1

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from .views import *
+urlpatterns = [
+    path('', home , name='home'),
+    path('accounts/login/', login_attempt , name='login_attempt'),
+    path('register', register_attempt , name='register_attempt'),
+    path('token', token_send, name='token_send'),
+    path('success', success , name='success'),
+    path('verify/<auth_token>' , verify , name="verify"),
+    path('error' , error_page , name="error"),
+    # path('accounts/logout',logout,name="logout")
+
+]
